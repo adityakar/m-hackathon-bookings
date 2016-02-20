8d80b256c09a492fc9f09697b35e65cce59c1bb9
Rakesh Menon <rmenon3@sapient.com>
2016-02-20 08:29:52 +0530

    hotel Facility API

---
+++ api/models/HotelFacility.js
@@ -0,0 +1,42 @@
+/**
+ * HotelFacility.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    hotelName: {
+      type: 'string',
+      required: true
+    },
+    facility: {
+      type: 'string',
+      required: true
+    },
+    openingHours: {
+      type: 'time',
+      required: true
+    },
+    closingHours: {
+      type: 'time',
+      required: true
+    }
+  },
+  validationMessages: {
+    hotelName: {
+      required: 'Hotel Name missing'
+    },
+    facility: {
+      required: 'Please provide facility name'
+    },
+    openingHours: {
+      required: 'Please specify the opening hours for the facility'
+    },
+    closingHours: {
+      required: 'Please specify the closing hours for the facility'
+    }
+  }
+};
+
